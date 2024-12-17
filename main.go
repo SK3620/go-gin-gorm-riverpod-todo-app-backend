71751176572d0db983f8c4833dec9c339e4f9c41
SK3620 <k.n.t11193620@gmail.com>
2024-12-17 15:15:25 +0900

    Update: ToDoモデルとToDoモデルのサンプルデータを作成

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"go-gin-gorm-riverpod-todo-app/models"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	// サンプルデータを作成
+	todos := []models.ToDo{
+		{ID: 1, Title: "タイトル１", IsCompleted: false},
+		{ID: 2, Title: "タイトル２", IsCompleted: true},
+		{ID: 3, Title: "タイトル３", IsCompleted: false},
+	}
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
---
+++ models/to_do.go
@@ -0,0 +1,7 @@
+package models
+
+type ToDo struct {
+	ID uint
+	Title string
+	IsCompleted bool
+}
