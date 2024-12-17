a75310bc33253738f92fb3bbc266999c8af38075
SK3620 <k.n.t11193620@gmail.com>
2024-12-17 23:10:56 +0900

    Update: DBマイグレーションを実行

---
+++ migrations/migration.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"go-gin-gorm-riverpod-todo-app/infra"
+	"go-gin-gorm-riverpod-todo-app/models"
+)
+
+func main() {
+	infra.Initialize()
+	db := infra.SetupDB()
+
+	if error := db.AutoMigrate(&models.Todo{}); error != nil {
+		panic("Failed to migrate database")
+	}
+}
